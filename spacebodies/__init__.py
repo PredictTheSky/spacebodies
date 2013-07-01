5eb29f583acb8cf0312ccb62ce377db543ca7c24
Nick Charlton <hello@nickcharlton.net>
2013-07-01 22:31:54 +0100

    Adds the *very* basics of the library.

---
+++ spacebodies/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+
+__title__ = 'spacebodies'
+__version__ = '0.0.1'
+__author__ = 'Nick Charlton'
+__licence__ = 'MIT'
+__copyright__  = 'Copyright 2013 The Predict the Sky Team'
