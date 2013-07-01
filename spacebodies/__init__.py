f2e6bdc4faa2249638848ebf6f1c29e19c36af10
Nick Charlton <hello@nickcharlton.net>
2013-07-01 23:30:36 +0100

    Adds imports for the Space Object classes.

--- spacebodies/__init__.py
+++ spacebodies/__init__.py
@@ -1,7 +1,21 @@
 # -*- coding: utf-8 -*-
 
+"""
+spacebodies
+~~~~~~~~~~~
+
+Space Objects combined with Weather Data. The library for Predict the Sky.
+
+TODO: Put an example here (at some point).
+
+"""
+
 __title__ = 'spacebodies'
 __version__ = '0.0.1'
 __author__ = 'Nick Charlton'
 __licence__ = 'MIT'
-__copyright__  = 'Copyright 2013 The Predict the Sky Team'
+__copyright__ = 'Copyright 2013 The Predict the Sky Team'
+
+from . import utils
+from .spacebodies import SpaceBodies
+from .spacebody import SpaceBody
