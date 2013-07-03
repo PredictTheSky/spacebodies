cbdea8a8c9614d84e7d7e93cdc025b67df64a7ea
Nick Charlton <hello@nickcharlton.net>
2013-07-03 11:31:44 +0100

    Simplifies the setup.py file.

--- setup.py
+++ setup.py
@@ -2,37 +2,31 @@
 
 import os
 import sys
+from setuptools import setup
 
 import spacebodies
-
-try:
-    from setuptools import setup
-except ImportError:
-    from distutils.core import setup
 
 if sys.argv[-1] == 'publish':
     os.system('python setup.py sdist upload')
     sys.exit()
 
-packages = ['spacebodies']
-
-requires = []
+install_requires = []
 
 setup(
     name='spacebodies',
     version=spacebodies.__version__,
     description='Space Objects combined with Weather Data',
-    long_description=open('README.rst').read() + '\n\n' +
-    open('HISTORY.rst').read(),
+    long_description='\n\n'.join([
+        open('README.rst').read(),
+        open('HISTORY.rst').read()
+    ]),
+    license=open('LICENSE').read(),
     author='Nick Charlton',
     author_email='info@predictthesky.org',
     url='http://predictthesky.org/',
-    packages=packages,
+    packages=['spacebodies'],
     package_data={'': ['LICENSE']},
     package_dir={'spacebodies': 'spacebodies'},
-    include_package_data=True,
-    install_requires=requires,
-    license=open('LICENSE').read(),
+    install_requires=install_requires,
     zip_safe=False,
-    classifiers=(),
 )
